3b121f6129e106137b4b265515c9bac6029d6e89
bob <siddharthbhalla98@gmail.com>
2018-03-30 23:54:47 +0530

    Added GooglePlaces Pod

--- iOScodingchallenge/Controller/ViewController.swift
+++ iOScodingchallenge/Controller/ViewController.swift
@@ -12,47 +12,15 @@ import GooglePlaces
 
 
 
-class ViewController: UIViewController , UISearchBarDelegate , GMSAutocompleteFetcherDelegate {
-
-    public func didFailAutocompleteWithError(_ error: Error) {
-    }
-
-    public func didAutocomplete(with predictions: [GMSAutocompletePrediction]) {
-    
-        
-        for prediction in predictions {
-            
-            if let prediction = prediction as GMSAutocompletePrediction!{
-                self.resultsArray.append(prediction.attributedFullText.string)
-            }
-        }
-        self.searchResultController.reloadDataWithArray(self.resultsArray)
-        print(resultsArray)
-    }
-    
-    
-    @IBOutlet weak var googleMapsContainer: UIView!
-    
-    var googleMapsView: GMSMapView!
-    var searchResultController: SearchResultsController!
-    var resultsArray = [String]()
-    var gmsFetcher: GMSAutocompleteFetcher!
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        
-        self.googleMapsView = GMSMapView(frame: self.googleMapsContainer.frame)
-        self.view.addSubview(self.googleMapsView)
-        
-        searchResultController = SearchResultsController()
-        searchResultController.delegate = self
-        gmsFetcher = GMSAutocompleteFetcher()
-        gmsFetcher.delegate = self
         
     }
 }
